478f147a28ad9afe54ed3ff0070b60fd171baa3e
Agus Richard <agus.richard21@gmail.com>
2020-02-23 04:39:54 +0000

    Fetch data and axios

--- react_net_ninja/poketimes/src/components/Home.js
+++ react_net_ninja/poketimes/src/components/Home.js
@@ -1,18 +1,47 @@
 import React from 'react';
+import axios from 'axios';
 import Rainbow from '../hoc/Rainbow';
 
-function Home() {
-  return (
-    <div className="container">
-      <h3 className="center">Home</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-        Vel officia, facilis inventore iure explicabo saepe numquam id 
-        laudantium iusto adipisci perspiciatis quibusdam reiciendis assumenda, 
-        quo tempora nobis culpa ex ipsam?
-      </p>
-    </div>
-  );
+class Home extends React.Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+        this.setState({
+          posts: res.data.slice(0, 10)
+        });
+      });
+  }
+
+  render() {
+    const { posts } = this.state;
+    let postsList = [];
+    if (posts.length) {
+      postsList = posts.map(post => {
+        return (
+          <div className="post card" key={ post.id }>
+            <div className="card-content">
+              <span className="card-title">{ post.title }</span>
+              <p className="card-body">{ post.body }</p>
+            </div>
+          </div>
+        );
+      });
+    } else {
+      postsList = <div className="center">There is no posts</div>
+    }
+
+    return (
+      <div className="container">
+        <h3 className="center">Home</h3>
+        <h4 className="center">Posts: </h4>
+        { postsList }
+      </div>
+    );
+  }
 }
 
-export default Rainbow(Home);
+export default Home;
