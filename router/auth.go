31f8f866132fdb1a748e0e499668732b6a9f0bd8
agusrichard <agus.richard21@gmail.com>
2020-08-19 17:22:02 +0700

    Login User

--- router/auth.go
+++ router/auth.go
@@ -1,8 +1,10 @@
 package router
 
 import (
+	"fmt"
 	"golang-restapi/models"
 	"golang-restapi/repository"
+	"golang-restapi/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +14,12 @@ func Register(c *gin.Context) {
 	if c.Request.Method == "POST" {
 		var user models.User
 		c.BindJSON(&user)
+		var err error
+		user.Password, err = utils.HashPassword(user.Password)
+		fmt.Printf("User password %v", user.Password)
+		if err != nil {
+			panic(err)
+		}
 		repository.CreateUser(&user)
 		c.JSON(200, gin.H{
 			"message": "Register Success",
@@ -30,12 +38,18 @@ func Login(c *gin.Context) {
 		var loginUser models.User
 		c.BindJSON(&loginUser)
 		var user models.User = repository.GetUserByUsername(loginUser.Username)
-		c.JSON(200, gin.H{
-			"message": "Login Success",
-			"data": gin.H{
-				"username": user.Username,
-				"password": user.Password,
-			},
-		})
+		if utils.CheckPasswordHash(loginUser.Password, user.Password) {
+			c.JSON(200, gin.H{
+				"message": "Login Success",
+				"data": gin.H{
+					"username": user.Username,
+					"password": user.Password,
+				},
+			})
+		} else {
+			c.JSON(400, gin.H{
+				"message": "Failed to login",
+			})
+		}
 	}
 }
