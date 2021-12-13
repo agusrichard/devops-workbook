71ff2a251330c9ca8f891588d06d26c9b3dcdfca
agusrichard <agus.richard21@gmail.com>
2021-12-13 22:12:52 +0700

    Using dynamodb local

---
+++ fastapi-dynamo/create_table.py
@@ -0,0 +1,45 @@
+import boto3
+def main():
+    # 1 - Create Client
+    ddb = boto3.resource('dynamodb',
+                         endpoint_url='http://localhost:8000',
+                         region_name='dummy',
+                         aws_access_key_id='dummy',
+                         aws_secret_access_key='dummy')
+    # 2 - Create the Table
+    ddb.create_table(TableName='Transactions',
+                     AttributeDefinitions=[
+                         {
+                             'AttributeName': 'TransactionId',
+                             'AttributeType': 'S'
+                         }
+                     ],
+                     KeySchema=[
+                         {
+                             'AttributeName': 'TransactionId',
+                             'KeyType': 'HASH'
+                         }
+                     ],
+                     ProvisionedThroughput= {
+                         'ReadCapacityUnits': 10,
+                         'WriteCapacityUnits': 10
+                     }
+                     )
+    print('Successfully created Table')
+
+    table = ddb.Table('Transactions')
+
+    input = {'TransactionId': '9a0', 'State': 'PENDING', 'Amount': 50}
+
+    #3 - Insert Data
+    table.put_item(Item=input)
+    print('Successfully put item')
+
+    #4 - Scan Table
+    scanResponse = table.scan(TableName='Transactions')
+    items = scanResponse['Items']
+    for item in items:
+        print(item)
+
+
+main()
--- fastapi-dynamo/main.py
+++ fastapi-dynamo/main.py
@@ -1,14 +1,11 @@
-"""
-Pylint told me to write in here
-"""
-
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 def index():
-    """
-    Pylint told me to write in here
-    """
     return 'Hello World! I am learning FastAPI, AWS DynamoDB, Docker and Kubernetes. Really excited to learn! Wohooo!!!'
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
