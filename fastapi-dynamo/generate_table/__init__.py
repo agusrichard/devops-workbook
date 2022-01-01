c36908ccff998b097cda0a3a97e353ada5915862
agusrichard <agus.richard21@gmail.com>
2022-01-01 23:30:31 +0700

    Notes to experiment

--- fastapi-dynamo/generate_table/__init__.py
+++ fastapi-dynamo/generate_table/__init__.py
@@ -3,8 +3,8 @@ import boto3
 def initialize_db():
     ddb = boto3.resource('dynamodb',
                          endpoint_url='http://localhost:8000',
-                         region_name='dummy',
-                         aws_access_key_id='dummy',
-                         aws_secret_access_key='dummy')
+                         region_name='example',
+                         aws_access_key_id='example',
+                         aws_secret_access_key='example')
 
     return ddb
