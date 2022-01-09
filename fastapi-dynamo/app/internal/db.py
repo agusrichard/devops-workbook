2a58543f3e8b620eee4827766011bf3cd278ad1c
agusrichard <agus.richard21@gmail.com>
2022-01-09 07:25:05 +0700

    Using env file to store sensitive data

--- fastapi-dynamo/app/internal/db.py
+++ fastapi-dynamo/app/internal/db.py
@@ -1,11 +1,25 @@
+import os
 import boto3
+import pathlib
+from dotenv import load_dotenv
 from boto3.resources.base import ServiceResource
 
+base_dir = pathlib.Path(__file__).parent.parent.parent
+
+load_dotenv(base_dir.joinpath('.env'))
+
+class Config:
+    DB_URL = os.getenv('DB_URL')
+    DB_REGION_NAME = os.getenv('DB_REGION_NAME')
+    DB_ACCESS_KEY_ID = os.getenv('DB_ACCESS_KEY_ID')
+    DB_SECRET_ACCESS_KEY = os.getenv('DB_SECRET_ACCESS_KEY')
+
 def initialize_db() -> ServiceResource:
+    print('Config', Config.DB_URL)
     ddb = boto3.resource('dynamodb',
-                         endpoint_url='http://localhost:8000',
-                         region_name='example',
-                         aws_access_key_id='example',
-                         aws_secret_access_key='example')
+                         endpoint_url=Config.DB_URL,
+                         region_name=Config.DB_REGION_NAME,
+                         aws_access_key_id=Config.DB_ACCESS_KEY_ID,
+                         aws_secret_access_key=Config.DB_SECRET_ACCESS_KEY)
 
     return ddb
