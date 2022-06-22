a05b5fc31ab3a90b50d429821ec1de6714edc8b4
Agus Richard <agus.richard21@gmail.com>
2022-06-22 23:42:35 +0700

    learn a bit more about serverless and AWS

--- try-serverless/handler.py
+++ try-serverless/handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def hello(event, context):
+def createTodo(event, context):
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
         "input": event,
