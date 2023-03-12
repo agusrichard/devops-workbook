44ff50e5820e716a69986432e05b28b58b42c147
Agus Richard <agus.richard21@gmail.com>
2023-03-12 21:56:53 +0700

    learn about Jenkinsfile

--- jenkins-tuts/main.py
+++ jenkins-tuts/main.py
@@ -6,6 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    print("Let's fly tomorrow")
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
