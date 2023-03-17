971c9bd2cdfcc21cdc97e76bd1fd03af631db3d8
Agus Richard <agus.richard21@gmail.com>
2023-03-17 23:18:44 +0700

    do I love her

--- jenkins-tuts/main.py
+++ jenkins-tuts/main.py
@@ -1,17 +1,6 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def love():
+    print('Do I love her?')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Let's fly tomorrow")
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    love()
