45cd01c9009ef3a445fec2a642b165fd3dbcbd0b
Agus Richard <agus.richard21@gmail.com>
2023-03-28 21:11:04 +0700

    forget about the girl

--- jenkins-tuts/main.py
+++ jenkins-tuts/main.py
@@ -1,13 +1,5 @@
 def love():
-    print('Do I love her?')
-    print('Probably')
-    print("to whom am I talking to?")
-    print("it sucks to love her")
-    print("forget about girl and focus on what you need learn and work")
-    print("tell me her name!")
-    print("Her name is Veni Alifa")
-    print("VENI ALIFA")
-    print("I have fever now, Veni! Will you take care of me?")
+    print("forget about the girl")
 
 
 if __name__ == '__main__':
